8aa35312c5da2d15b95e1f995de5cc1d5b83fdda
Donna Harris <donnajharris@gmail.com>
2022-04-01 21:47:17 -0400

    works-ish for n=2 (no solutions)

--- p2.py
+++ p2.py
@@ -2,6 +2,7 @@
 
 
 import math
+from re import T
 
 count = 0
 
@@ -13,112 +14,97 @@ by providing appropriate implementation for the IsSolution, ConstructCandidates,
 '''
 
 
-# From Lesson 13 - Backtracking
-
-
-'''
-
-Input:
-	A : queen i, is placed in row i and column A[i]
-	k:  number of queens placed so far
-	S : set of input values, (not used)
-
-def IsSolution(A, k, S)
-1. return k > Length(A)	# each queen is placed without conflict 
-      and we do not use array index 0
-
-def ConstructCandidates(A, k, S)
-1. Result = []		# empty array, will contain the column 
-                        indexes where the k-th queen has no threat
-2. for i = 1 to Length(A):	# try each column i in row k
-3.   hasThreat = False
-4.   for j = 1 to k-1:	# check all the previously placed queen in rows 1 to k-1
-5.     if i == A[i] 	# if queen ir row j has same column as i	
-         or abs(k-j) == abs(i-A[j): # or, queens are in the same diagonal means delta row 
-	    (i.e., abs(k-j)) == delta column (i.e., abs(i-A[j])
-6.     hasThreat = True # queen [k, i] and [j, A[j]] are in same column or diagonal
-7.   if not hasThreat:
-8.     Result.Append(i)
-9. return Result
-
-def Process(A, k, S)
-1. global count = count + 1	# Count the number of solution in a global variable count. 
-                  used by IsFinished callback
-2. Print(A)
-
-def IsFinished()
-1. return count > 0		# Assuming we want to find any one solution
-
-'''
-
 def IsSolution(A, k, S):
     
-    print("\tIsSolution() where k =", k, " is greater than the LENGTH of A =", A[1:len(A)], "? {0}\n".format(k > len(A)-1) )
+    print("\tIsSolution( {0}, {1} ) - where k > len(A)? {1} > {2}? {3}\n".format(A, k, len(A), k > len(A)) )
     
-    return k > len(A)-1
+    return k > len(A)
 
+
+def HasThreatInColumn(A, col_index_of_candidate, row_of_existing_queen):
+    
+    result = A[row_of_existing_queen] == col_index_of_candidate
+    
+    if result:
+        print("\t\t\tThere is a threat in the same column.")
+    
+    return result
+  
+
+def HasThreatOnDiagonal(A, i, k, j):
+    
+    # two points are on the same diagonal, if either
+    #   y_2 - y_1 == x_2 - x_1    (postive slope)
+    #   y_2 - y_1 == x_1 - x_2    (negative slope)
+    
+    # y values are like the rows  (k and j)
+    # x values are like the columns  (i and A[row_index])
+    
+    # 1 is the placed queen
+    # 2 is the candidate placement of the next queen
+    
+    result = abs(j-k) == abs(i-A[j])
+    
+    if result:
+        print("\t\t\tThere is a threat on one of the diagonals.")
+    
+    return result
+        
+    
 def ConstructCandidates(A, k, S):
     Result = []
     
-    print("\tConstructCandidates()")
-    #print("\t- for Queen",k)
+    print("\tConstructCandidates()\t- for Queen in Row k =",k)
     
+    # i is column...
     for i in range(1, len(A)):
         hasThreat = False
         
-        #print("\t\ti=",i)
+        print("\t\tcolumn i=",i)
         
-        for j in range(1, k-1):  
-            #print("\t\t\tj=",j)
-    
+        # looking at ***PREVIOUSLY*** placed queens
+        for j in range(1, k): # k-1):  this SHOULD be k, because the upper range is NOT inclusive
+
+            print("\t\t\tlooking at row j = ",j)
+                
             # IF queen in row j has same column as i 
             # OR queens are in the same diagonal means delta row == delta column 
-            if j == A[i] or abs(k-j) == abs(i-A[j]):
-                #print("\t\t\t\t{0} == {1} OR abs({3}-{2}) == abs({0}-{4})".format(i, A[i], j, k, A[j]))
+                        
+            if HasThreatInColumn(A, i, j) or HasThreatOnDiagonal(A, i, k, j):
+            #if i == A[i] or abs(k-j) == abs(i-A[j]):
+                print("\t\tFound a threat...")
                 hasThreat = True
+                
                 
         if not hasThreat:
             #print("\t\t\tQueen {0} offers no threat to Queen {1}.".format(i,k))
             Result.append(i)
+        else:
+            print()
+            #print("Threat... returning nuthin'")
             
             
-    print("\n\t\tCC is returning Result = ",Result,"\n")
+    print("\n\t\tReturning Result = ",Result,"\n")
             
     return Result
 
 
 def Process(A, k, S):
-    print("\tProcess() - showing that we've been doing the work.")
+    print("\tProcess() - ")
     global count
-    count = count + 1
-    print("\t\tA ==--- Solution ---== >>>>> ",A)
+    count = count + 1   # increment the number of solutions found!
+    print("\t\t==--- Solution Found ---== >>>>> ",A)
 
 
 def IsFinished():
     print("\tIsFinished() - Is count ({0}) > 0? {1}".format(count, count > 0))
+    
+    # assuming we only want to return 1 result, otherwise always return False
     return count > 0
 
 
-'''
-
-def Backtrack(A, k, S)
-	Input:
-		A : Current partial candidate solution 
-		k:  Current position in A
-		S : set of input values
-1. if IsSolution(A, k, S):
-2.    Process(A, k, S)
-3. else:
-4.    L = ConstructCandidates(A, k, S)
-5.    for each candidate c in L:
-6.       a[k+1] = c
-7.       Backtrack(A, k+1, S)
-8.       if Finished():
-9.          return
-
-'''
 def Backtrack(A, k, S):
-    print("\nBacktrack(",A[1:len(A)],", ",k,")")
+    print("\nBacktrack(",A,", ",k,")")
           
     if IsSolution(A, k, S):
         Process(A, k, S)
@@ -127,19 +113,24 @@ def Backtrack(A, k, S):
         
         for c in L:
             
-            print("\t\tc =",c," and len(A) is = " + str(len(A)) + " and k is = " + str(k))
-            
-            if k+1 < len(A):
-                A[k+1] = c
+            # if k+1 < len(A):
+            A[k+1] = c
+                #print("\t\t\tA[k+1] is set to c")
+            #else:
+            #    print("\t\t\tk+1 is bigger than len(A), so we're gonna hit the stop")
             Backtrack(A, k+1, S)
                 
             if IsFinished():
-                return            
+                return True
+            else:
+                print("\tStill not done looking for solutions.\n")   
+                
+    return False      
             
 
 def main():
     
-    n = 4   # NOTE, n must be 4 or greater for the n-queens problem
+    n = 2   # NOTE, n must be 4 or greater for the n-queens problem
     
     print("Hello! n =",n)
     
@@ -148,6 +139,7 @@ def main():
     S = None # unused
     
     result = Backtrack(A, k, S)
+    print("One or more solutions found? ",result,"\n")
     
 
 
