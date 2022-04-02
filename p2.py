9ac6474d3b070ccbc7972ac8209c360dcadc5c08
Donna Harris <donnajharris@gmail.com>
2022-04-02 19:36:24 -0400

    The correct answer except for A[0]

--- p2.py
+++ p2.py
@@ -2,7 +2,7 @@
 
 
 import math
-from re import T
+import copy
 
 count = 0
 
@@ -18,19 +18,21 @@ by providing appropriate implementation for the IsSolution, ConstructCandidates,
 
 def IsSolution(A, k, S):
     
-    print("\tIsSolution( {0}, {1} ) - where k > len(A)? {1} > {2}? {3}\n".format(A, k, len(A), k > len(A)) )
+    print(S,"\tIsSolution( {0}, {1} ) - where k > len(A)-1? {1} > {2}? {3}\n".format(A, k, len(A)-1, k > len(A)-1) )
     
-    return k > len(A)
+    return k > len(A) -1
 
 
-def HasThreatInColumn(A, col_index_of_candidate, row_of_existing_queen):
+def HasThreatInColumn(A, col_index_of_candidate_i, row_of_existing_queen_j):
     
-    result = A[row_of_existing_queen] == col_index_of_candidate
+    result = A[row_of_existing_queen_j] == col_index_of_candidate_i
     
     if result:
         print("\t\t\tThere is a threat in the same column.")
     
     return result
+
+
   
 
 def HasThreatOnDiagonal(A, i, k, j):
@@ -51,58 +53,71 @@ def HasThreatOnDiagonal(A, i, k, j):
         print("\t\t\tThere is a threat on one of the diagonals.")
     
     return result
+
+
+def ThreatsAreFound(A, i, j, k):
+    
+    # TODO:  fix this to be the right CHECKS
+    #print("ALERT - only checking for same row/col")
+    
+    return HasThreatInColumn(A, i, j) or HasThreatOnDiagonal(A, i, k, j)
+    #return HasThreatInColumn(A, i, j)
+    
         
     
 def ConstructCandidates(A, k, S):
     Result = []
     
-    print("\tConstructCandidates()\t- for Queen in Row k =",k)
+    print(S,"\tConstructCandidates()\t- for Queen in Row k =",k)
     
     # i is column...
     for i in range(1, len(A)):
         hasThreat = False
         
-        print("\t\tcolumn i=",i)
+        print(S,"\t\tcolumn i=",i)
         
         # looking at ***PREVIOUSLY*** placed queens
         for j in range(1, k): # k-1):  this SHOULD be k, because the upper range is NOT inclusive
 
-            print("\t\t\tlooking at row j = ",j)
+            print(S,"\t\t\tlooking at row j = ",j)
                 
             # IF queen in row j has same column as i 
             # OR queens are in the same diagonal means delta row == delta column 
                         
-            if HasThreatInColumn(A, i, j) or HasThreatOnDiagonal(A, i, k, j):
-            #if i == A[i] or abs(k-j) == abs(i-A[j]):
-                print("\t\tFound a threat...")
+            #if HasThreatInColumn(A, i, j) or HasThreatOnDiagonal(A, i, k, j):
+            if ThreatsAreFound(A, i, j, k):
                 hasThreat = True
-                
+
+            
+            #if i == A[i] or abs(k-j) == abs(i-A[j]):
+                #print("\t\tFound a threat...")
+  
                 
         if not hasThreat:
-            #print("\t\t\tQueen {0} offers no threat to Queen {1}.".format(i,k))
+
             Result.append(i)
-        else:
-            print()
-            #print("Threat... returning nuthin'")
+        # else:
+        #     print("\t\t\tBreak - experiment")
+        #     break
             
             
-    print("\n\t\tReturning Result = ",Result,"\n")
+    print("\n",S,"\t\tReturning Result = ",Result,"\n")
             
     return Result
 
 
 def Process(A, k, S):
-    print("\tProcess() - ")
+    print("S,\tProcess() - ")
     global count
     count = count + 1   # increment the number of solutions found!
-    print("\t\t==--- Solution Found ---== >>>>> ",A)
+    print(S,"\t\t==--- Solution Found ---== >>>>> ",A)
     global solutions
-    solutions.append(A)
+    #solutions.append(A)
+
+    solutions.append(copy.copy(A))
 
 
-def IsFinished():
-    #print("\tIsFinished() - Is count ({0}) > 0? {1}".format(count, count > 0))
-    
+def IsFinished():    
     # assuming we only want to return 1 result, otherwise always return False
     #return count > 0
     
@@ -110,7 +125,7 @@ def IsFinished():
 
 
 def Backtrack(A, k, S):
-    print("\nBacktrack(",A,", ",k,")")
+    print("\n",S,"Backtrack(",A,", ",k,")")
           
     if IsSolution(A, k, S):
         Process(A, k, S)
@@ -119,38 +134,62 @@ def Backtrack(A, k, S):
         
         for c in L:
             
-            
-            
-            if k+1 < len(A):
-                A[k+1] = c
-                print("\t\tKeep going...!")
-            #else:
-            #    print("\t\t\tk+1 is bigger than len(A), so we're gonna hit the stop")
+            #if k+1 < len(A):
+            A[k] = c
+                #print("\t\tKeep going...!")
+
+            S = S + "\t"
             Backtrack(A, k+1, S)
                 
             # if IsFinished():
             #     return True
             # else:
-            #     print("\tStill not done looking for solutions.\n")   
-                
-    # return False      
+            #     print("\tStill not done looking for a solution.\n")      
             
+
+def Test_HasThreatInColumn():
+    # def HasThreatInColumn(A, col_index_of_candidate_i, row_of_existing_queen_j):
+    
+    # k is the candidate queen... k in the A array is the candidate loctaion
+    #       I THINK we're checking to see if that candidate IS a solution
+    
+    # A[j] == i ?
+
+    A = [0, 0, 0, 0, 0]
+    #for row in range(1,len(test)):
+        # print("row (j?) =", row)
+        # print(HasThreatInColumn(test, 2, row))
+        
+    # HasThreadInColumn(A, column of  , row of existing queen being checked (k-1))
+    print(HasThreatInColumn(A, 1, 1))
+    print(HasThreatInColumn(A, 2, 1))
+    print(HasThreatInColumn(A, 3, 1))
+    print(HasThreatInColumn(A, 4, 1))
+    print(HasThreatInColumn(A, 5, 1))
+    
+    
+    
 
 def main():
     
-    n = 4   # NOTE, n must be 4 or greater for the n-queens problem
+    n = 4  # NOTE, n must be 4 or greater for the n-queens problem
+ 
+    
+    #return
+    
     
     print("Hello! n =",n)
     
     A = [0] * (n+1)
     k = 0
     S = None # unused
+    S = "" # using as spacing for now
     
     result = Backtrack(A, k, S)
-    #print("One or more solutions found? ",result,"\n")
     
     global solutions
     print("Set of all solutions =\n" + str(solutions))
+    print("Number of possible solutions? ",len(solutions),"\n")
     
 
 
